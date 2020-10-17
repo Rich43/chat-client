19bcc0a5be09b4f7c0d50d6a880dba9ef8ad4747
Richie Ward <RichieS@GMail.com>
2020-10-17 14:12:54 +0100

    remove junk from proxy, update schema

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,16 +3,8 @@ module.exports = function(app) {
     app.use(
         '/graphql',
         createProxyMiddleware({
-            target: 'https://localhost:8443',
+            target: 'http://localhost:8080',
             changeOrigin: true,
-            secure: false
-        })
-    );
-    app.use(
-        '/auth',
-        createProxyMiddleware({
-            target: 'http://localhost:8180',
-            changeOrigin: false,
             secure: false
         })
     );
