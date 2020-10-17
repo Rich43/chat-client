997983dc0afbe7aa4d90236d666e60927b7aa011
Richie Ward <RichieS@GMail.com>
2020-10-17 15:27:34 +0100

    load initial data

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Box, Button, TextField } from "@material-ui/core";
 import { useCreateMessageMutation } from "../graphql/createMessage";
 import {makeStyles} from "@material-ui/core/styles";
+import {useListMessagesQuery} from "../graphql/listMessages";
 
 const useStyles = makeStyles({
     chatLog: {
@@ -12,7 +13,14 @@ const useStyles = makeStyles({
 export function Home() {
     const classes = useStyles();
     const [message, setMessage] = useState('');
+    const ref = useRef<HTMLTextAreaElement>(null);
     const [createMessage] = useCreateMessageMutation();
+    const listMessages = useListMessagesQuery();
+    useEffect(() => {
+        if (ref.current && listMessages.data) {
+            ref.current.value = ''.concat(...listMessages.data.listMessages.map(row => `[${row.created}] ${row.message}\n`));
+        }
+    }, [listMessages]);
     const onSubmit = () => {
         console.log(message);
         createMessage(
@@ -26,6 +34,7 @@ export function Home() {
             <Box display="flex" flexDirection="column" flex="1">
                 <textarea
                     className={classes.chatLog}
+                    ref={ref}
                 />
                 <Box display="flex" flexDirection="row" alignSelf="flex-end">
                     <TextField
