2434402e7dd294ac3591be0c5f3d73c6fa767114
Richie Ward <RichieS@GMail.com>
2023-11-22 22:27:53 +0000

    Revert "Use new API"
    This reverts commit f038375d1b8eda14057744550a6228e2bd15f22a.

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,12 +1,14 @@
-import { OperationDefinitionNode } from "graphql";
+import {OperationDefinitionNode} from "graphql";
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { getMainDefinition } from '@apollo/client/utilities';
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
 
-const wsLink = new GraphQLWsLink(createClient({
-    url: `ws${window.location.protocol.toLowerCase().startsWith('https') ? 's' : ''}://${window.location.host}/subscriptions`
-}));
+const wsLink = new WebSocketLink({
+    uri: `ws${window.location.protocol.toLowerCase().startsWith('https') ? 's' : ''}://${window.location.host}/subscriptions`,
+    options: {
+        reconnect: true
+    }
+});
 const httpLink = new HttpLink({
     uri: '/graphql',
 });
